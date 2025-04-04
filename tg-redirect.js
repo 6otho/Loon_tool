0d574599ca0cc6b44d282baaaef845e905ddcae4
6otho <124158269+6otho@users.noreply.github.com>
2025-04-04 14:07:26 +0800

    Update tg-redirect.js

--- tg-redirect.js
+++ tg-redirect.js
@@ -1,27 +1,25 @@
 // tg-redirect.js
 
 if (typeof $request !== 'undefined') {
-  // 获取传入的 jump 参数，默认使用 "sg"
-  let jump = ($argument && $argument.jump) ? $argument.jump.toLowerCase().trim() : "sg";
+  // 获取传入的 jump 参数，默认为空
+  let jump = $argument.jump || "";
   let url = $request.url;
-
+  
   // 判断是否为 Telegram 链接
   if (/^(https:\/\/)?t\.me\/(.+)/.test(url)) {
     let redirectUrl = "";
-
+    
     if (jump === "sg") {
-      // 将 Telegram 链接转换为 swiftgram 协议链接
-      // 例如：https://t.me/xxx -> swiftgram://resolve?domain=xxx
+      // 例如将 https://t.me/xxx 转换为 swiftgram://resolve?domain=xxx
       redirectUrl = url.replace(/^(https:\/\/)?t\.me\/(.+)/, "swiftgram://resolve?domain=$2");
     } else if (jump === "turrit") {
-      // 将 Telegram 链接转换为 turrit 协议链接
-      // 例如：https://t.me/xxx -> turrit://$2
+      // 例如转换为 turrit://xxx（根据实际客户端协议进行修改）
       redirectUrl = url.replace(/^(https:\/\/)?t\.me\/(.+)/, "turrit://$2");
     } else {
-      // 参数异常时直接使用原链接
+      // 若没有选择或者参数异常，直接跳转原链接
       redirectUrl = url;
     }
-
+    
     // 返回 302 重定向响应
     $done({
       response: {
@@ -32,6 +30,7 @@ if (typeof $request !== 'undefined') {
       }
     });
   } else {
+    // 如果 URL 不匹配，直接返回请求不作处理
     $done({});
   }
 }
